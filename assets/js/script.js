fb9ed8cfc8e71ca7be9a421c07c630f129446e91
Tommy Boileau <tommyboileau@Tommys-MacBook-Air.local>
2022-08-12 18:39:14 -0700

    added the date to the currentDay id with a native date function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 var timeBlock = $(".timeBlock");
+var saveBtn = $(".saveBtn");
+
+//create a native .date to read the current date/time and display it to the currentDay id
+const d = new Date();
+let text = d.toString();
+document.getElementById("currentDay").innerHTML = text;
 
 function saveSchedule() {
   timeBlock.textarea;
